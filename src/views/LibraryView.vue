5ffa000efc2a59bcd29bcf44884906ca9a96fb6a
PPOSM1993 <pedro.osorio04@inacapmail.cl>
2025-02-06 22:10:29 -0300

    Inicio Clon de Spotify con Vue3

---
+++ src/views/LibraryView.vue
@@ -0,0 +1,28 @@
+
+<script>
+//import SongRow from '../components/SongRow.vue'
+import Play from 'vue-material-design-icons/Play.vue';
+import Pause from 'vue-material-design-icons/Pause.vue';
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+import Heart from 'vue-material-design-icons/Heart.vue';
+import ClockTimeThreeOutline from 'vue-material-design-icons/ClockTimeThreeOutline.vue';
+
+import artist from '../artist.json'
+
+</script>
+
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+        xD
+        </button>
+
+        <div class="py-1.5"></div>
+
+    </div>
+</template>
+
